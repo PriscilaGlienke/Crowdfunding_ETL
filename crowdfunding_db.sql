6d464a3127e8103ea73f14cd310ffd42a3c978dd
PriscilaGlienke <priglienke@gmail.com>
2023-05-18 17:37:42 -0400

    changes

---
+++ crowdfunding_db.sql
@@ -0,0 +1,39 @@
+CREATE TABLE contacts (
+	contact_id INT PRIMARY KEY NOT NULL,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	email VARCHAR(255)
+);
+
+CREATE TABLE category (
+	category_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	category VARCHAR(255)
+);
+
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	subcategory VARCHAR(255)
+);
+
+CREATE TABLE campaign (
+	cf_id INT PRIMARY KEY NOT NULL,
+	contact_id INT REFERENCES contacts(contact_id) NOT NULL,
+	company_name VARCHAR(255),
+	description VARCHAR(255),
+	goal FLOAT,
+	pledge FLOAT,
+	outcome VARCHAR(10),
+	backers_count INT,
+	country VARCHAR(10),
+	currency VARCHAR(10),
+	launched_date DATE,
+	end_date DATE,
+	category_id VARCHAR(10) REFERENCES category(category_id) NOT NULL,
+	subcategory_id VARCHAR(10) REFERENCES subcategory(subcategory_id) NOT NULL
+);
+
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM campaign;
+
--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,33 +1,62 @@
-CREATE TABLE contacts (
-	contact_id INT PRIMARY KEY NOT NULL,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	email VARCHAR(255)
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
-CREATE TABLE category (
-	category_id VARCHAR(10) PRIMARY KEY NOT NULL,
-	category VARCHAR(255)
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
-CREATE TABLE subcategory (
-	subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
-	subcategory VARCHAR(255)
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
-CREATE TABLE campaign (
-	cf_id INT PRIMARY KEY NOT NULL,
-	contact_id INT REFERENCES contacts(contact_id) NOT NULL,
-	company_name VARCHAR(255),
-	description VARCHAR(255),
-	goal FLOAT,
-	pledge FLOAT,
-	outcome VARCHAR(10),
-	backers_count INT,
-	country VARCHAR(10),
-	currency VARCHAR(10),
-	launched_date DATE,
-	end_date DATE,
-	category_id VARCHAR(10) REFERENCES category(category_id) NOT NULL,
-	subcategory_id VARCHAR(10) REFERENCES subcategory(subcategory_id) NOT NULL
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(10)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
 );
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
